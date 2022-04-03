0433fe19b78dee0f0f3b0469257a543d22b27610
Andronomos <andronomos@inbox.com>
2022-04-03 12:32:17 -0700

    Removed irrelevant tools
    Removed hammer and excavator tools.

--- src/main/java/andronomos/andromobtools/registry/ModItems.java
+++ src/main/java/andronomos/andromobtools/registry/ModItems.java
@@ -3,8 +3,6 @@ package andronomos.andromobtools.registry;
 import andronomos.andromobtools.AndroMobTools;
 import andronomos.andromobtools.item.*;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Tier;
-import net.minecraft.world.item.Tiers;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -20,29 +18,11 @@ public class ModItems {
     public static final RegistryObject<Item> FAKE_SWORD = ITEMS.register("fake_sword", () -> new FakeSword());
     public static final RegistryObject<Item> PORTABLE_LOOT_ATTRACTOR = ITEMS.register("portable_loot_attractor", () -> new PortableLootAttractorItem(BASE_PROPERTIES));
 
-    public static final RegistryObject<HammerItem> IRON_HAMMER = registerHammer("iron", Tiers.IRON);
-    public static final RegistryObject<HammerItem> GOLD_HAMMER = registerHammer("gold", Tiers.GOLD);
-    public static final RegistryObject<HammerItem> DIAMOND_HAMMER = registerHammer("diamond", Tiers.DIAMOND);
-    public static final RegistryObject<HammerItem> NETHERITE_HAMMER = registerHammer("netherite", Tiers.NETHERITE);
-
-    public static final RegistryObject<ExcavatorItem> IRON_EXCAVATOR = registerExcavator("iron", Tiers.IRON);
-    public static final RegistryObject<ExcavatorItem> GOLD_EXCAVATOR = registerExcavator("gold", Tiers.GOLD);
-    public static final RegistryObject<ExcavatorItem> DIAMOND_EXCAVATOR = registerExcavator("diamond", Tiers.DIAMOND);
-    public static final RegistryObject<ExcavatorItem> NETHERITE_EXCAVATOR = registerExcavator("netherite", Tiers.NETHERITE);
-
     //private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, Supplier<T> block) {
     //    return ITEMS.register(name, () -> new BlockItem(block.get(), BASE_PROPERTIES));
     //}
 
     private static RegistryObject<Item> register(String name) {
         return ITEMS.register(name, () -> new Item(BASE_PROPERTIES));
-    }
-
-    private static RegistryObject<HammerItem> registerHammer(String name, Tier tier) {
-        return ITEMS.register(name + "_hammer", () -> new HammerItem(tier, BASE_PROPERTIES));
-    }
-
-    private static RegistryObject<ExcavatorItem> registerExcavator(String name, Tier tier) {
-        return ITEMS.register(name + "_excavator", () -> new ExcavatorItem(tier, BASE_PROPERTIES));
     }
 }
