44cdc119ecb382bade3a586eab447915ad8d7281
tim-dm <andronomos@inbox.com>
2022-05-14 12:06:02 -0700

    .

--- src/main/java/andronomos/androtech/data/ModRecipeProvider.java
+++ src/main/java/andronomos/androtech/data/ModRecipeProvider.java
@@ -6,7 +6,6 @@ import net.minecraft.data.*;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.data.recipes.ShapedRecipeBuilder;
-import net.minecraft.data.recipes.ShapelessRecipeBuilder;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
