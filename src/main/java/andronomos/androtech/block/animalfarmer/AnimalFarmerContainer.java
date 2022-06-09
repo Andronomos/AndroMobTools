b7946cdab02bbf39f28e8a5dfac5a2b38facf588
tim-dm <andronomos@inbox.com>
2022-06-09 13:04:17 -0700

    Update AnimalFarmerContainer.java
    Added slots

--- src/main/java/andronomos/androtech/block/animalfarmer/AnimalFarmerContainer.java
+++ src/main/java/andronomos/androtech/block/animalfarmer/AnimalFarmerContainer.java
@@ -1,5 +1,6 @@
 package andronomos.androtech.block.animalfarmer;
 
+import andronomos.androtech.Const;
 import andronomos.androtech.block.BaseContainerMenu;
 import andronomos.androtech.block.cropfarmer.CropFarmerBE;
 import andronomos.androtech.registry.ModBlocks;
@@ -8,12 +9,29 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ContainerLevelAccess;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.SlotItemHandler;
 
 public class AnimalFarmerContainer extends BaseContainerMenu {
-	public final CropFarmerBE blockEntity;
+	public final BlockEntity blockEntity;
 
 	public AnimalFarmerContainer(int windowId, BlockPos pos, Inventory inventory) {
 		super(ModContainers.ANIMAL_FARMER.get(), windowId, inventory);
+
+		blockEntity = this.player.getCommandSenderWorld().getBlockEntity(pos);
+
+		if(blockEntity != null) {
+			blockEntity.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY).ifPresent(h -> {
+				for (int i = 0; i < 6; i++) {
+					for(int j = 0; j < 9; j++) {
+						addSlot(new SlotItemHandler(h, j + i * 9, Const.CONTAINER_SLOT_X_OFFSET + j * Const.SCREEN_SLOT_SIZE, Const.SCREEN_SLOT_SIZE + i * Const.SCREEN_SLOT_SIZE));
+					}
+				}
+			});
+		}
+
+		layoutPlayerInventorySlots(8, 140);
 	}
 
 	@Override
