b45a32ea1fde988f701b57c8a210b4a6c25caf53
tim-dm <andronomos@inbox.com>
2022-06-09 12:49:06 -0700

    Update CropFarmerScreen.java
    Removed unnecessary resourcelocation

--- src/main/java/andronomos/androtech/block/cropfarmer/CropFarmerScreen.java
+++ src/main/java/andronomos/androtech/block/cropfarmer/CropFarmerScreen.java
@@ -1,21 +1,17 @@
 package andronomos.androtech.block.cropfarmer;
 
-import andronomos.androtech.AndroTech;
 import andronomos.androtech.block.MachineScreen;
 import andronomos.androtech.gui.widget.button.machinebutton.PowerButton;
 import andronomos.androtech.network.AndroTechPacketHandler;
 import andronomos.androtech.network.packet.SyncMachinePoweredState;
 import andronomos.androtech.registry.TextureRegistry;
 import com.mojang.blaze3d.vertex.PoseStack;
-import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
 
 public class CropFarmerScreen extends MachineScreen<CropFarmerContainer> {
 	private PowerButton powerButton;
 	private CropFarmerContainer container;
-	public static final ResourceLocation CROP_FARMER_GUI = new ResourceLocation(AndroTech.MOD_ID, "textures/gui/machine/crop_farmer.png");
 
 	public CropFarmerScreen(CropFarmerContainer container, Inventory inventory, Component component) {
 		super(container, inventory, component);
