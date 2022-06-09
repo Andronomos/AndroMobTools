bbd0c4fcb9a024956ca3954939ab1d241ee55a0b
tim-dm <andronomos@inbox.com>
2022-06-09 13:25:25 -0700

    Update TextureRegistry.java

--- src/main/java/andronomos/androtech/registry/TextureRegistry.java
+++ src/main/java/andronomos/androtech/registry/TextureRegistry.java
@@ -7,6 +7,7 @@ public class TextureRegistry {
 	public static final ResourceLocation INVENTORY = new ResourceLocation(AndroTech.MOD_ID, "textures/gui/inventory.png");
 	public static final ResourceLocation INVENTORY_PLAIN = new ResourceLocation(AndroTech.MOD_ID, "textures/gui/inventory_plain.png");
 	public static final ResourceLocation INVENTORY_LARGE = new ResourceLocation(AndroTech.MOD_ID, "textures/gui/inventory_large.png");
+	public static final ResourceLocation INVENTORY_LARGE_PLAIN = new ResourceLocation(AndroTech.MOD_ID, "textures/gui/inventory_large_plain.png");
 	public static final ResourceLocation SLOT = new ResourceLocation(AndroTech.MOD_ID, "textures/gui/slot.png");
 	public static final ResourceLocation SLOT_UNIT = new ResourceLocation(AndroTech.MOD_ID, "textures/gui/slot_unit.png");
 	public static final ResourceLocation SLOT_ENCHANTED_BOOK = new ResourceLocation(AndroTech.MOD_ID, "textures/gui/slot_enchanted_book.png");
