9399e8bde93743ed8e68012835749ccbc26dc908
tim-dm <andronomos@inbox.com>
2022-06-19 12:05:59 -0700

    Update AnimalFarmer.java
    Added directional state

--- src/main/java/andronomos/androtech/block/animalfarmer/AnimalFarmer.java
+++ src/main/java/andronomos/androtech/block/animalfarmer/AnimalFarmer.java
@@ -4,6 +4,7 @@ import andronomos.androtech.block.AndroTechTickingMachine;
 import andronomos.androtech.block.cropfarmer.CropFarmerBE;
 import andronomos.androtech.block.cropfarmer.CropFarmerContainer;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
@@ -11,19 +12,58 @@ import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.Mirror;
+import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraftforge.network.NetworkHooks;
 import org.jetbrains.annotations.Nullable;
 
 public class AnimalFarmer  extends AndroTechTickingMachine {
 	public static final String SCREEN_ANIMAL_FARMER = "screen.androtech.animal_farmer";
+	public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
 
 	public AnimalFarmer(Properties properties) {
-		super(properties);
+		super(properties, true, true, false, true);
+	}
+
+	@javax.annotation.Nullable
+	@Override
+	public BlockState getStateForPlacement(BlockPlaceContext context) {
+		BlockState state = super.getStateForPlacement(context);
+
+		for(final Direction facing : context.getNearestLookingDirections()) {
+			if(facing.getAxis().isHorizontal()) {
+				state = state.setValue(FACING, facing.getOpposite());
+				break;
+			}
+		}
+
+		return state;
+	}
+
+	@Override
+	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+		super.createBlockStateDefinition(builder);
+		builder.add(FACING);
+	}
+
+	@Override
+	public BlockState rotate(BlockState state, Rotation rotation) {
+		return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
+	}
+
+	@Override
+	public BlockState mirror(BlockState state, Mirror mirror) {
+		return state.setValue(FACING, mirror.mirror(state.getValue(FACING)));
 	}
 
 	@Nullable
