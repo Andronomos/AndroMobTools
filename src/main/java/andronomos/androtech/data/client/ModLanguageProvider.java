604a55a82b922b01e47f53f695fb5cc6a0b5e951
tim-dm <andronomos@inbox.com>
2022-06-26 18:17:30 -0700

    Update ModLanguageProvider.java

--- src/main/java/andronomos/androtech/data/client/ModLanguageProvider.java
+++ src/main/java/andronomos/androtech/data/client/ModLanguageProvider.java
@@ -1,6 +1,7 @@
 package andronomos.androtech.data.client;
 
-import andronomos.androtech.block.ItemIncinerator.ItemIncinerator;
+import andronomos.androtech.block.itemdeconstructor.ItemDeconstructor;
+import andronomos.androtech.block.itemincinerator.ItemIncinerator;
 import andronomos.androtech.block.amethystharvester.AmethystHarvester;
 import andronomos.androtech.block.animalfarmer.AnimalFarmer;
 import andronomos.androtech.block.blockminer.BlockMiner;
@@ -67,16 +68,31 @@ public class ModLanguageProvider extends LanguageProvider {
 		add(ModItems.FLUID_REMOVER.get(), "Fluid Remover");
 		add(FluidRemover.FLUID_REMOVER_MODE, "Mode: ");
 
-		add(RedstoneTransmitter.name, "Redstone Transmitter");
-		add(MobCloner.name, "Mob Cloner");
-		add(CropFarmer.name, "Crop Farmer");
-		add(ItemMender.name, "Item Mender");
-		add(MobKillingPad.name, "Mob Killing Pad");
-		add(ItemIncinerator.name, "Item Incinerator");
-		add(ItemAttractor.name, "Item Attractor");
-		add(AnimalFarmer.name, "Animal Farmer");
-		add(AmethystHarvester.name, "Amethyst Harvester");
-		add(BlockMiner.name, "Block Miner");
+		add(RedstoneTransmitter.NAME, "Redstone Transmitter");
+		add(MobCloner.NAME, "Mob Cloner");
+		add(CropFarmer.NAME, "Crop Farmer");
+		add(ItemMender.NAME, "Item Mender");
+		add(MobKillingPad.NAME, "Mob Killing Pad");
+		add(ItemIncinerator.NAME, "Item Incinerator");
+		add(ItemAttractor.NAME, "Item Attractor");
+		add(AnimalFarmer.NAME, "Animal Farmer");
+		add(AmethystHarvester.NAME, "Amethyst Harvester");
+		add(BlockMiner.NAME, "Block Miner");
+		add(ItemDeconstructor.NAME, "Item Deconstructor");
+		add(ItemDeconstructor.STATUS_INVENTORYFULL, "Not enough inventory space");
+		add(ItemDeconstructor.STATUS_NORECIPE, "No recipe found");
+
+		add("block.androtech.mob_cloner.tooltip", "Spawns a copies of mobs placed inside it");
+		add("block.androtech.item_attractor.tooltip", "Collects nearby items");
+		add("block.androtech.item_incinerator.tooltip", "Deletes items inserted into it");
+		add("block.androtech.animal_farmer.tooltip", "Shears sheep and milks cows");
+		add("block.androtech.crop_farmer.tooltip", "Automatically harvests most crops");
+		add("block.androtech.item_mender.tooltip", "Repairs items placed inside it");
+		add("block.androtech.redstone_transmitter.tooltip", "");
+		add("block.androtech.redstone_receiver.tooltip", "");
+		add("block.androtech.amethyst_harvester.tooltip", "Automatically harvests nearby amethyst clusters");
+		add("block.androtech.block_miner.tooltip", "Mines the block placed directly in front of it");
+		add("block.androtech.item_deconstructor.tooltip", "Breaks down items into their ingredients");
 
 		add("gui.androtech.powered_on", "Powered On");
 		add("gui.androtech.powered_off", "Powered Off");
