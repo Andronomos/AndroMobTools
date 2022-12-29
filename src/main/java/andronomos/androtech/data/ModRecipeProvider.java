69c89dc09bc0b6b9d6a79942202883a485557491
tim-dm <andronomos@inbox.com>
2022-12-28 17:10:53 -0800

    Update ModRecipeProvider.java
    Added Block GPS Module recipe

--- src/main/java/andronomos/androtech/data/ModRecipeProvider.java
+++ src/main/java/andronomos/androtech/data/ModRecipeProvider.java
@@ -22,6 +22,7 @@ public class ModRecipeProvider extends RecipeProvider {
 
         createChipRecipe(ModItems.BASIC_CHIP.get(), Items.GOLD_INGOT, consumer);
         createChipRecipe(ModItems.ADVANCED_CHIP.get(), Items.DIAMOND, consumer);
+        createBasicModuleRecipe(ModItems.BLOCK_GPS_MODULE.get(), ModItems.BASIC_CHIP.get(), consumer);
     }
 
     private void createChipRecipe(Item chip, Item material, Consumer<FinishedRecipe> consumer) {
@@ -34,6 +35,18 @@ public class ModRecipeProvider extends RecipeProvider {
                 .pattern("212")
                 .pattern("343")
                 .unlockedBy("has_item", has(material))
+                .save(consumer);
+    }
+
+    private void createBasicModuleRecipe(Item outputItem, Item item, Consumer<FinishedRecipe> consumer) {
+        ShapedRecipeBuilder.shaped(outputItem)
+                .define('1', Items.IRON_INGOT)
+                .define('2', Items.PAPER)
+                .define('3', item)
+                .pattern("121")
+                .pattern("232")
+                .pattern("121")
+                .unlockedBy("has_item", has(Items.LAVA_BUCKET))
                 .save(consumer);
     }
 }
