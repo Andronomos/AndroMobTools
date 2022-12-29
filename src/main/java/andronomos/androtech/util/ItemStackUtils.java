f4ba80f86c4098768c215190ad5e4cc4f4137e9b
tim-dm <andronomos@inbox.com>
2022-12-28 17:09:26 -0800

    Update ItemStackUtils.java
    Added entity methods for the MobStasisModule

--- src/main/java/andronomos/androtech/util/ItemStackUtils.java
+++ src/main/java/andronomos/androtech/util/ItemStackUtils.java
@@ -4,10 +4,13 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
 
 public class ItemStackUtils {
 	public static void applyDamage(LivingEntity player, ItemStack stack, int amount, boolean preventBreaking) {
@@ -44,5 +47,21 @@ public class ItemStackUtils {
 		}
 
 		return new BlockPos(xPos, yPos, zPos);
+	}
+
+	public static boolean hasEntityTag(ItemStack stack) {
+		return !stack.isEmpty() && stack.hasTag() && stack.getTag().contains("entity");
+	}
+
+	@Nullable
+	public static Entity getEntity(ItemStack stack, Level world, boolean withInfo) {
+		EntityType type = EntityType.byString(stack.getTag().getString("entity")).orElse(null);
+		if (type != null) {
+			Entity entity = type.create(world);
+			if (withInfo)
+				entity.load(stack.getTag());
+			return entity;
+		}
+		return null;
 	}
 }
