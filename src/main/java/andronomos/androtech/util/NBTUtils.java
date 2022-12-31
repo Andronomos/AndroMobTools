4dc622dae84a050764031a1c96bfff8835672ac2
tim-dm <andronomos@inbox.com>
2022-12-31 09:00:30 -0800

    Update NBTUtils.java

--- src/main/java/andronomos/androtech/util/NBTUtils.java
+++ src/main/java/andronomos/androtech/util/NBTUtils.java
@@ -4,18 +4,18 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.item.ItemStack;
 
 public class NBTUtils {
-	public static CompoundTag getStackTag(ItemStack itemStackIn) {
-		if(itemStackIn.getTag() == null) {
+	public static CompoundTag getStackTag(ItemStack stack) {
+		if(stack.getTag() == null) {
 			return new CompoundTag();
 		}
-		return itemStackIn.getTag();
+		return stack.getTag();
 	}
 
-	public static void setIntVal(ItemStack item, String prop, int value) {
-		item.getOrCreateTag().putInt(prop, value);
+	public static void setIntVal(ItemStack stack, String prop, int value) {
+		stack.getOrCreateTag().putInt(prop, value);
 	}
 
-	public static void setStringVal(ItemStack item, String prop, String value) {
-		item.getOrCreateTag().putString(prop, value);
+	public static void setStringVal(ItemStack stack, String prop, String value) {
+		stack.getOrCreateTag().putString(prop, value);
 	}
 }
