b61a6c49c78870cad64c6a235c6f769586c82d86
tim-dm <andronomos@inbox.com>
2022-12-31 08:59:44 -0800

    Update ToggleableDevice.java

--- src/main/java/andronomos/androtech/item/base/ToggleableDevice.java
+++ src/main/java/andronomos/androtech/item/base/ToggleableDevice.java
@@ -1,11 +1,9 @@
 package andronomos.androtech.item.base;
 
-import andronomos.androtech.util.ItemStackUtils;
 import andronomos.androtech.util.NBTUtils;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
