f2ee1dc9f5f96fe4cdc6ae6489a27e74004327d3
Andronomos <andronomos@proton.me>
2023-12-23 00:20:50 -0800

    initial 1.20 port

---
+++ src/main/java/andronomos/androtech/AndroTech.java
@@ -0,0 +1,30 @@
+package andronomos.androtech;
+
+import andronomos.androtech.registry.ModBlocks;
+import andronomos.androtech.registry.ModItems;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(AndroTech.MODID)
+public class AndroTech {
+	public static final String MODID = "androtech";
+	public static final Logger LOGGER = LogManager.getLogger(andronomos.androtech.AndroTech.MODID);
+
+	public AndroTech() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		ModBlocks.BLOCKS.register(modEventBus);
+		ModItems.ITEMS.register(modEventBus);
+		MinecraftForge.EVENT_BUS.register(this);
+
+		modEventBus.addListener(this::clientSetup);
+	}
+
+	private void clientSetup(final FMLClientSetupEvent event) {
+
+	}
+}
---
+++ src/main/java/andronomos/androtech/registry/ModItems.java
@@ -0,0 +1,10 @@
+package andronomos.androtech.registry;
+
+import andronomos.androtech.AndroTech;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModItems {
+	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, AndroTech.MODID);
+}
