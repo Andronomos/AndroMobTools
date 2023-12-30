e54b34e303b52574ca3108bdbd023d6094393097
Andronomos <andronomos@proton.me>
2023-12-30 06:02:40 -0800

    Added restricted slot class for handling slots that only accept a certain item

---
+++ src/main/java/andronomos/androtech/inventory/RestrictedSlot.java
@@ -0,0 +1,33 @@
+package andronomos.androtech.inventory;
+
+import net.minecraft.world.Container;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
+
+import javax.annotation.Nonnull;
+
+public class RestrictedSlot extends Slot {
+	ItemStack item;
+	int maxItems;
+
+	public RestrictedSlot(Container inventory, int slotIndex, int x, int y, ItemStack item, int maxItems) {
+		super(inventory, slotIndex, x, y);
+		this.item = item;
+		this.maxItems = maxItems;
+	}
+
+	@Override
+	public boolean mayPlace(ItemStack stack) {
+		return stack.getItem() == item.getItem() && stack.getDamageValue() == item.getDamageValue();
+	}
+
+	@Override
+	public int getMaxStackSize() {
+		return maxItems;
+	}
+
+	@Override
+	public int getMaxStackSize(@Nonnull ItemStack stack) {
+		return this.getMaxStackSize();
+	}
+}
