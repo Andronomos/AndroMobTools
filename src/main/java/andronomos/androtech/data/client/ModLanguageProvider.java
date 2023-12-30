d3c97b6216e9584f8abbe425e1a00304011ad481
Andronomos <andronomos@proton.me>
2023-12-29 20:42:57 -0800

    Cleaned up logic

--- src/main/java/andronomos/androtech/data/client/ModLanguageProvider.java
+++ src/main/java/andronomos/androtech/data/client/ModLanguageProvider.java
@@ -1,15 +1,17 @@
 package andronomos.androtech.data.client;
 
 import andronomos.androtech.AndroTech;
-import andronomos.androtech.registry.ModBlocks;
-import andronomos.androtech.registry.ModCreativeTabs;
+import andronomos.androtech.block.pad.damagepad.DamagePadBlock;
+import andronomos.androtech.registry.BlockRegistry;
+import andronomos.androtech.registry.CreativeTabRegistry;
 import net.minecraft.data.PackOutput;
 import net.minecraftforge.common.data.LanguageProvider;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-import org.apache.commons.lang3.StringUtils;
 
+import java.util.Arrays;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class ModLanguageProvider extends LanguageProvider {
 	public ModLanguageProvider(PackOutput output, String locale) {
@@ -18,13 +20,22 @@ public class ModLanguageProvider extends LanguageProvider {
 
 	@Override
 	protected void addTranslations() {
-		add("creativetab." + ModCreativeTabs.BASETABNAME, "AndroTech");
-
-		ModBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get).forEach(b -> {
+		BlockRegistry.BLOCKS.getEntries().stream().map(RegistryObject::get).forEach(b -> {
 			String name = Objects.requireNonNull(ForgeRegistries.BLOCKS.getKey(b)).getPath();
 			name = name.replaceAll("_", " ");
-			name = StringUtils.capitalize(name);
+			name = capitalizeWords(name);
 			add(b, name);
 		});
+
+		add("creativetab." + CreativeTabRegistry.BASETABNAME, "AndroTech");
+
+		add(DamagePadBlock.DISPLAY_NAME, "Damage Pad");
+		add(DamagePadBlock.TOOLTIP, "Damages entities that stand on it");
+	}
+
+	static String capitalizeWords(String input) {
+		return Arrays.stream(input.split("\\s+"))
+				.map(word -> word.substring(0, 1).toUpperCase() + word.substring(1))
+				.collect(Collectors.joining(" "));
 	}
 }
