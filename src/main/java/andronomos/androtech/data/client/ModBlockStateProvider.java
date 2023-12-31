d0d34d7629d4f5527abaaaa3f5d008a2dbc667db
Andronomos <andronomos@proton.me>
2023-12-31 07:07:11 -0800

    Added machine blockstate datagen

--- src/main/java/andronomos/androtech/data/client/ModBlockStateProvider.java
+++ src/main/java/andronomos/androtech/data/client/ModBlockStateProvider.java
@@ -1,13 +1,16 @@
 package andronomos.androtech.data.client;
 
 import andronomos.androtech.AndroTech;
+import andronomos.androtech.block.base.machine.ATMachineBlock;
 import andronomos.androtech.block.pad.PadBlock;
 import andronomos.androtech.block.pad.PadEffectBlock;
 import andronomos.androtech.block.pad.RotatablePadBlock;
 import andronomos.androtech.registry.BlockRegistry;
 import net.minecraft.core.Direction;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.client.model.generators.ConfiguredModel;
 import net.minecraftforge.client.model.generators.ModelFile;
@@ -24,16 +27,69 @@ public class ModBlockStateProvider extends BlockStateProvider {
 	protected void registerStatesAndModels() {
 		BlockRegistry.BLOCKS.getEntries().stream().map(RegistryObject::get).forEach(b -> {
 			String blockName = ForgeRegistries.BLOCKS.getKey(b).getPath();
-			String blockType = b.getClass().getSimpleName();
-			String textureFolder = blockName;
 
 			if(b instanceof PadBlock) {
 				registerPadStateAndModel(b, blockName + "_top");
+			} else if (b instanceof ATMachineBlock machineBlock) {
+				registerMachineBlockState(machineBlock);
 			} else {
-				registerBlockStateAndModel(b, blockName, textureFolder);
+				registerBlockStateAndModel(b, blockName, blockName);
 			}
 		});
 	}
+
+	private void registerMachineBlockState(ATMachineBlock machine) {
+		String machineName = ForgeRegistries.BLOCKS.getKey(machine).getPath();
+
+		if (machine.hasMultipleStates) {
+			getVariantBuilder(machine).forAllStates(state -> {
+				boolean isPowered = state.getValue(BlockStateProperties.POWERED);
+
+				String stateTextureKey = "_off";
+				String topTexture = String.format("block/%s", machine.textures.get(machine.textures.containsKey("top_off") ? "top_off" : "top"));
+				String bottomTexture = String.format("block/%s", machine.textures.get(machine.textures.containsKey("bottom_off") ? "bottom_off" : "bottom"));
+				String sideTexture = String.format("block/%s", machine.textures.get(machine.textures.containsKey("side_off") ? "side_off" : "side"));
+
+				if (isPowered) {
+					stateTextureKey = "_on";
+					topTexture = String.format("block/%s", machine.textures.get(machine.textures.containsKey("top_on") ? "top_on" : "top"));
+					bottomTexture = String.format("block/%s", machine.textures.get(machine.textures.containsKey("bottom_on") ? "bottom_on" : "bottom"));
+					sideTexture = String.format("block/%s", machine.textures.get(machine.textures.containsKey("side_on") ? "side_on" : "side"));
+				}
+
+				ResourceLocation sideResource = modLoc(sideTexture);
+				ModelFile model = models().cube(machineName + stateTextureKey,
+								modLoc(bottomTexture), modLoc(topTexture),
+								sideResource, sideResource, sideResource, sideResource)
+						.texture("particle", sideResource);
+
+				return ConfiguredModel.builder()
+						.modelFile(model)
+						.build();
+			});
+
+			itemModels().withExistingParent(machineName, modLoc("block/" + machineName + "_off"));
+		} else {
+			String topTexture = String.format("block/%s", machine.textures.get("top"));
+			String bottomTexture = String.format("block/%s", machine.textures.get("bottom"));
+			String sideTexture = String.format("block/%s", machine.textures.get("side"));
+			String frontTexture = String.format("block/%s", machine.textures.get("front"));
+
+			ModelFile model = models().cube(ForgeRegistries.BLOCKS.getKey(machine).getPath(),
+					modLoc(bottomTexture),
+					modLoc(topTexture),
+					modLoc(frontTexture),
+					modLoc(sideTexture),
+					modLoc(sideTexture),
+					modLoc(sideTexture))
+					.texture("particle", sideTexture);
+
+			simpleBlock(machine, model);
+			itemModels().withExistingParent(machineName, modLoc("block/" + machineName));
+		}
+	}
+
+
 
 	private void registerPadStateAndModel(Block block, String top) {
 		String blockName = ForgeRegistries.BLOCKS.getKey(block).getPath();
@@ -47,19 +103,12 @@ public class ModBlockStateProvider extends BlockStateProvider {
 			getVariantBuilder(block).forAllStatesExcept(state -> {
 				Direction direction = state.getValue(PadEffectBlock.FACING);
 
-				int yRot = 0;
-
-				switch (direction) {
-					case EAST:
-						yRot = 90;
-						break;
-					case WEST:
-						yRot = 270;
-						break;
-					case SOUTH:
-						yRot = 180;
-						break;
-				}
+				int yRot = switch (direction) {
+					case EAST -> 90;
+					case WEST -> 270;
+					case SOUTH -> 180;
+					default -> 0;
+				};
 
 				return ConfiguredModel.builder()
 						.modelFile(model)
