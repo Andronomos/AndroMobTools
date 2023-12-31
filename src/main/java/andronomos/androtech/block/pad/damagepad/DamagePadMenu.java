c86eb637cdd243935ae133cdda93f7a5dde29e63
Andronomos <andronomos@proton.me>
2023-12-30 20:53:24 -0800

    Updated restricted slots

--- src/main/java/andronomos/androtech/block/pad/damagepad/DamagePadMenu.java
+++ src/main/java/andronomos/androtech/block/pad/damagepad/DamagePadMenu.java
@@ -1,12 +1,11 @@
 package andronomos.androtech.block.pad.damagepad;
 
 import andronomos.androtech.block.base.BaseMenu;
-import andronomos.androtech.inventory.RestrictedSlot;
+import andronomos.androtech.inventory.RestrictedSlotHandler;
 import andronomos.androtech.registry.BlockRegistry;
 import andronomos.androtech.registry.ItemRegistry;
 import andronomos.androtech.registry.MenuTypeRegistry;
 import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ContainerData;
@@ -14,10 +13,9 @@ import net.minecraft.world.inventory.ContainerLevelAccess;
 import net.minecraft.world.inventory.SimpleContainerData;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.items.SlotItemHandler;
 
 public class DamagePadMenu extends BaseMenu {
-	public DamagePadBlockEntity blockEntity;
-
 	public DamagePadMenu(int containerId, Inventory inventory, FriendlyByteBuf extraData) {
 		this(containerId, inventory, inventory.player.level().getBlockEntity(extraData.readBlockPos()), new SimpleContainerData(inventory.getContainerSize()));
 	}
@@ -27,13 +25,14 @@ public class DamagePadMenu extends BaseMenu {
 		addPlayerInventory();
 		addPlayerHotbar();
 		if(entity instanceof DamagePadBlockEntity damagePadBlockEntity) {
-			this.blockEntity = damagePadBlockEntity;
 			damagePadBlockEntity.getCapability(ForgeCapabilities.ITEM_HANDLER).ifPresent(iItemHandler -> {
-				addSlot(new RestrictedSlot(new SimpleContainer(1), 0, 30, 30, ItemRegistry.DAMAGE_PAD_UPGRADE_SHARPNESS.get().getDefaultInstance(), 10));
-				addSlot(new RestrictedSlot(new SimpleContainer(1), 0, 55, 30, ItemRegistry.DAMAGE_PAD_UPGRADE_LOOTING.get().getDefaultInstance(), 10 ));
-				addSlot(new RestrictedSlot(new SimpleContainer(1), 0, 80, 30, ItemRegistry.DAMAGE_PAD_UPGRADE_FIRE.get().getDefaultInstance(), 10 ));
-				addSlot(new RestrictedSlot(new SimpleContainer(1), 0, 105, 30, ItemRegistry.DAMAGE_PAD_UPGRADE_SMITE.get().getDefaultInstance(), 10 ));
-				addSlot(new RestrictedSlot(new SimpleContainer(1), 0, 130, 30, ItemRegistry.DAMAGE_PAD_UPGRADE_ARTHRO.get().getDefaultInstance(), 10 ));
+				//addSlot(new SlotItemHandler(iItemHandler, 0, 30, 30));
+
+				addSlot(new RestrictedSlotHandler(iItemHandler, 0, 30, 30, ItemRegistry.DAMAGE_PAD_UPGRADE_SHARPNESS.get().getDefaultInstance(), 10));
+				addSlot(new RestrictedSlotHandler(iItemHandler, 1, 55, 30, ItemRegistry.DAMAGE_PAD_UPGRADE_LOOTING.get().getDefaultInstance(), 10 ));
+				addSlot(new RestrictedSlotHandler(iItemHandler, 2, 80, 30, ItemRegistry.DAMAGE_PAD_UPGRADE_FIRE.get().getDefaultInstance(), 10 ));
+				addSlot(new RestrictedSlotHandler(iItemHandler, 3, 105, 30, ItemRegistry.DAMAGE_PAD_UPGRADE_SMITE.get().getDefaultInstance(), 10 ));
+				addSlot(new RestrictedSlotHandler(iItemHandler, 4, 130, 30, ItemRegistry.DAMAGE_PAD_UPGRADE_ARTHRO.get().getDefaultInstance(), 10 ));
 			});
 		}
 		setSlotIndexes(DamagePadBlock.PAD_SLOTS);
