a9eaeb5291f476829070af24395095fe90c92f2b
Andronomos <andronomos@proton.me>
2023-12-31 08:24:10 -0800

    .

---
+++ src/main/java/andronomos/androtech/Constants.java
@@ -0,0 +1,22 @@
+package andronomos.androtech;
+
+public class Constants {
+	public static final int PLAYER_HOTBAR_SLOT_COUNT = 9;
+	public static final	int PLAYER_HOTBAR_MENU_Y_OFFSET = 142;
+	public static final int PLAYER_INVENTORY_MENU_Y_OFFSET = 84;
+	public static final int PLAYER_INVENTORY_ROW_COUNT = 3;
+	public static final int PLAYER_INVENTORY_COLUMN_COUNT = 9;
+	public static final int PLAYER_INVENTORY_SLOT_COUNT = PLAYER_INVENTORY_ROW_COUNT * PLAYER_INVENTORY_COLUMN_COUNT + PLAYER_HOTBAR_SLOT_COUNT;
+
+	public static final int VANILLA_INVENTORY_SLOT_COUNT = 27;
+	public static final int VANILLA_LARGE_INVENTORY_SLOT_COUNT = 54;
+
+	public static final int MENU_SLOT_X_OFFSET = 8;
+	public static final int MENU_SLOT_Y_OFFSET = 16;
+	public static final int MENU_SLOT_X_CENTER = 80;
+	public static final int MENU_SLOT_SIZE = 16;
+	public static final int SCREEN_SLOT_X_OFFSET = 7;
+	public static final int SCREEN_SLOT_X_CENTER = 79;
+	public static final int SCREEN_SLOT_SIZE = 18;
+	public static final int SCREEN_IMAGE_HEIGHT_LARGE = 222;
+}
