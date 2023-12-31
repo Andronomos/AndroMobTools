40dbe4046b1b1de667e6a0fd3aa6dc90415a4804
Andronomos <andronomos@proton.me>
2023-12-30 21:09:29 -0800

    Removed unused import

--- src/main/java/andronomos/androtech/inventory/RestrictedSlotHandler.java
+++ src/main/java/andronomos/androtech/inventory/RestrictedSlotHandler.java
@@ -1,6 +1,5 @@
 package andronomos.androtech.inventory;
 
-import net.minecraft.world.Container;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.SlotItemHandler;
