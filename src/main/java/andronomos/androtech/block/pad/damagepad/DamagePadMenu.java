e18e9a484995f31596030fa36453bb586686e4a3
Andronomos <andronomos@proton.me>
2024-01-01 05:14:53 -0800

    Remove the quickMoveStack method from the DamagePad menu

--- src/main/java/andronomos/androtech/block/pad/damagepad/DamagePadMenu.java
+++ src/main/java/andronomos/androtech/block/pad/damagepad/DamagePadMenu.java
@@ -2,6 +2,7 @@ package andronomos.androtech.block.pad.damagepad;
 
 import andronomos.androtech.block.base.BaseMenu;
 import andronomos.androtech.inventory.server.RestrictedSlotHandler;
+import andronomos.androtech.item.DamagePadUpgradeItem;
 import andronomos.androtech.registry.BlockRegistry;
 import andronomos.androtech.registry.ItemRegistry;
 import andronomos.androtech.registry.MenuTypeRegistry;
@@ -11,8 +12,13 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ContainerData;
 import net.minecraft.world.inventory.ContainerLevelAccess;
 import net.minecraft.world.inventory.SimpleContainerData;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraftforge.common.capabilities.ForgeCapabilities;
+
+import static andronomos.androtech.Constants.*;
 
 public class DamagePadMenu extends BaseMenu {
 	public DamagePadMenu(int containerId, Inventory inventory, FriendlyByteBuf extraData) {
@@ -25,42 +31,15 @@ public class DamagePadMenu extends BaseMenu {
 		addPlayerHotbar();
 		if(entity instanceof DamagePadBlockEntity damagePadBlockEntity) {
 			damagePadBlockEntity.getCapability(ForgeCapabilities.ITEM_HANDLER).ifPresent(itemHandler -> {
-				addSlot(new RestrictedSlotHandler(itemHandler, 0, 43, 30, ItemRegistry.SHARPNESS_AUGMENT.get().getDefaultInstance(), 10));
-				addSlot(new RestrictedSlotHandler(itemHandler, 1, 68, 30, ItemRegistry.LOOTING_AUGMENT.get().getDefaultInstance(), 10 ));
-				addSlot(new RestrictedSlotHandler(itemHandler, 2, 93, 30, ItemRegistry.FIRE_AUGMENT.get().getDefaultInstance(), 10 ));
-				addSlot(new RestrictedSlotHandler(itemHandler, 3, 118, 30, ItemRegistry.SMITE_AUGMENT.get().getDefaultInstance(), 10 ));
+				addSlot(new RestrictedSlotHandler(itemHandler, 0, 43, 30, ItemRegistry.SHARPNESS_AUGMENT.get().getDefaultInstance(), DamagePadBlock.AUGMENT_STACK_LIMIT));
+				addSlot(new RestrictedSlotHandler(itemHandler, 1, 68, 30, ItemRegistry.LOOTING_AUGMENT.get().getDefaultInstance(), DamagePadBlock.AUGMENT_STACK_LIMIT));
+				addSlot(new RestrictedSlotHandler(itemHandler, 2, 93, 30, ItemRegistry.FIRE_AUGMENT.get().getDefaultInstance(), DamagePadBlock.AUGMENT_STACK_LIMIT));
+				addSlot(new RestrictedSlotHandler(itemHandler, 3, 118, 30, ItemRegistry.SMITE_AUGMENT.get().getDefaultInstance(), DamagePadBlock.AUGMENT_STACK_LIMIT));
 			});
 		}
 		setSlotIndexes(DamagePadBlock.PAD_SLOTS);
 		//addDataSlots(data);
 	}
-
-	//@Override
-	//public ItemStack quickMoveStack(Player player, int slotIndex) {
-	//	Slot sourceSlot = slots.get(slotIndex);
-	//	if (sourceSlot == null || !sourceSlot.hasItem()) return ItemStack.EMPTY;
-	//	ItemStack sourceStack = sourceSlot.getItem();
-	//	ItemStack sourceStackCopy = sourceStack.copy();
-	//
-	//	//if(slotIndex >= playerInventoryFirstSlot && sourceStack.getItem() instanceof SwordItem) {
-	//	//	if (!this.moveItemStackTo(sourceStack, blockEntityFirstSlot, blockEntityLastSlot, false)) {
-	//	//		return ItemStack.EMPTY;
-	//	//	}
-	//	//} else {
-	//	//	if(!moveItemStackTo(sourceStack, playerInventoryFirstSlot, playerInventoryLastSlot, false)) {
-	//	//		return ItemStack.EMPTY;
-	//	//	}
-	//	//}
-	//
-	//	// If stack size == 0 (the entire stack was moved) set slot contents to null
-	//	if (sourceStack.getCount() == 0) {
-	//		sourceSlot.set(ItemStack.EMPTY);
-	//	} else {
-	//		sourceSlot.setChanged();
-	//	}
-	//	sourceSlot.onTake(player, sourceStack);
-	//	return sourceStackCopy;
-	//}
 
 	@Override
 	public boolean stillValid(Player player) {
