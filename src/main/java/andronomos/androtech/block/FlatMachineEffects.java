fc33fb055bce701310f6668e176fbae1ba6e9567
Andronomos <andronomos@proton.me>
2024-01-01 12:49:44 -0800

    Fixed jump collision effect using wrong class

--- src/main/java/andronomos/androtech/block/FlatMachineEffects.java
+++ src/main/java/andronomos/androtech/block/FlatMachineEffects.java
@@ -1,6 +1,7 @@
 package andronomos.androtech.block;
 
 import andronomos.androtech.block.collisioneffect.CollisionEffectAcceleration;
+import andronomos.androtech.block.collisioneffect.CollisionEffectJumpBoost;
 import andronomos.androtech.block.collisioneffect.ICollisionEffect;
 
 public class FlatMachineEffects {
@@ -8,5 +9,5 @@ public class FlatMachineEffects {
 	public static ICollisionEffect ACCELERATION_NORMAL = new CollisionEffectAcceleration(0.3d);
 	public static ICollisionEffect ACCELERATION_STRONG = new CollisionEffectAcceleration(1.5d);
 
-	public static ICollisionEffect JUMP_WEAK = new CollisionEffectAcceleration(1.5d);
+	public static ICollisionEffect JUMP_WEAK = new CollisionEffectJumpBoost(1.5d);
 }
