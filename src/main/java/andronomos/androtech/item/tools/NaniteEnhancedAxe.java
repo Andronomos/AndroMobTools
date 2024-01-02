dd661cc43e15a224edefb7c9ba86284656ec702f
Andronomos <andronomos@proton.me>
2024-01-01 23:03:56 -0800

    Removed whitespace from the nanite enhanced tools inventoryTick method

--- src/main/java/andronomos/androtech/item/tools/NaniteEnhancedAxe.java
+++ src/main/java/andronomos/androtech/item/tools/NaniteEnhancedAxe.java
@@ -25,15 +25,12 @@ public class NaniteEnhancedAxe extends AxeItem {
 	@Override
 	public void inventoryTick(ItemStack stack, Level level, Entity entity, int itemSlot, boolean isSelected) {
 		if (level.isClientSide || !(entity instanceof Player)) return;
-
 		if(this.tickCounter == this.tickDelay) {
 			this.tickCounter = 0;
-
 			if(stack.isDamaged()) {
 				stack.setDamageValue(stack.getDamageValue() - AndroTechConfig.NANITE_REPAIR_RATE.get());
 			}
 		}
-
 		this.tickCounter++;
 	}
 }
