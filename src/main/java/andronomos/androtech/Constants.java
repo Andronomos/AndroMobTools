3d3222087be9fda594a799e086db5097a44d71a3
Andronomos <andronomos@proton.me>
2024-01-01 22:47:42 -0800

    Added constants

--- src/main/java/andronomos/androtech/Constants.java
+++ src/main/java/andronomos/androtech/Constants.java
@@ -20,4 +20,43 @@ public class Constants {
 	public static final int SCREEN_SLOT_X_CENTER = 79;
 	public static final int SCREEN_SLOT_SIZE = 18;
 	public static final int SCREEN_IMAGE_HEIGHT_LARGE = 222;
+
+	public class EnchantmentLevel {
+		public final static int I = 1;
+		public final static int II = 2;
+		public final static int III = 3;
+		public final static int IV = 4;
+		public final static int V = 5;
+	}
+
+	public class EffectAmplifier {
+		public final static int I = 0;
+		public final static int II = 1;
+		public final static int III = 2;
+		public final static int IV = 3;
+		public final static int V = 4;
+	}
+
+	public class TicksInSeconds {
+		public final static int ONE = 20;
+		public final static int TWO = 40;
+		public final static int THREE = 60;
+		public final static int FOUR = 80;
+		public final static int FIVE = 100;
+		public final static int TEN = 200;
+	}
+
+	public class TicksInMinutes {
+		public final static int ONE = 1200;
+		public final static int TWO = 2400;
+		public final static int THREE = 3600;
+		public final static int FIVE = 6000;
+		public final static int TEN = 1200;
+		public final static int TWENTY = 2400;
+		public final static int THIRTY = 3600;
+	}
+
+	public class TicksInHours {
+		public final static int ONE = 7200;
+	}
 }
