fbcde0d53bbfb068c6acee2d5118b0bf8d771a7a
Andronomos <andronomos@proton.me>
2024-01-01 22:52:06 -0800

    Registered nanite enhanced tool

--- src/main/java/andronomos/androtech/registry/ItemRegistry.java
+++ src/main/java/andronomos/androtech/registry/ItemRegistry.java
@@ -3,13 +3,20 @@ package andronomos.androtech.registry;
 import andronomos.androtech.AndroTech;
 import andronomos.androtech.item.DamagePadUpgradeItem;
 import andronomos.androtech.item.FakeSword;
+import andronomos.androtech.item.tools.NaniteEnhancedAxe;
+import andronomos.androtech.item.tools.NaniteEnhancedPickAxe;
+import andronomos.androtech.item.tools.NaniteEnhancedShovel;
+import andronomos.androtech.item.tools.NaniteEnhancedSword;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Tiers;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ItemRegistry {
+	public static final Item.Properties NANITE_TOOL_PROPERTIES = new Item.Properties().fireResistant();
+
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, AndroTech.MODID);
 
 	public static final RegistryObject<Item> FAKE_SWORD = ITEMS.register("fake_sword",
@@ -26,4 +33,16 @@ public class ItemRegistry {
 
 	public static final RegistryObject<Item> SMITE_AUGMENT = ITEMS.register("smite_augment",
 			() -> new DamagePadUpgradeItem(new Item.Properties(), Enchantments.SMITE));
+
+	public static final RegistryObject<Item> NANITE_ENHANCED_PICKAXE = ITEMS.register("nanite_enhanced_pickaxe",
+			() -> new NaniteEnhancedPickAxe(Tiers.NETHERITE, 1, -2.8F, new Item.Properties().fireResistant()));
+
+	public static final RegistryObject<Item> NANITE_ENHANCED_AXE = ITEMS.register("nanite_enhanced_axe",
+			() -> new NaniteEnhancedAxe(Tiers.NETHERITE, 5.0F, -3.0F, NANITE_TOOL_PROPERTIES));
+
+	public static final RegistryObject<Item> NANITE_ENHANCED_SHOVEL = ITEMS.register("nanite_enhanced_shovel",
+			() -> new NaniteEnhancedShovel(Tiers.NETHERITE, 5.0F, -3.0F, NANITE_TOOL_PROPERTIES));
+
+	public static final RegistryObject<Item> NANITE_ENHANCED_SWORD = ITEMS.register("nanite_enhanced_sword",
+			() -> new NaniteEnhancedSword(Tiers.NETHERITE, 3, -2.4F, NANITE_TOOL_PROPERTIES));
 }
