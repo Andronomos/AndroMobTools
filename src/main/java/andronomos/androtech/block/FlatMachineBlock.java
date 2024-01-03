207d991492a4a56962a855595d0f70c585dfe5a3
Andronomos <andronomos@proton.me>
2024-01-02 19:44:36 -0800

    .

--- src/main/java/andronomos/androtech/block/FlatMachineBlock.java
+++ src/main/java/andronomos/androtech/block/FlatMachineBlock.java
@@ -9,6 +9,7 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.NotNull;
 
 public class FlatMachineBlock extends MachineBlock {
 	protected static final VoxelShape SHAPE = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 1.0D, 16.0D);
@@ -22,33 +23,32 @@ public class FlatMachineBlock extends MachineBlock {
 	}
 
 	@Override
-	public VoxelShape getShape(BlockState state, BlockGetter getter, BlockPos pos, CollisionContext context) {
+	public @NotNull VoxelShape getShape(@NotNull BlockState state, @NotNull BlockGetter getter, @NotNull BlockPos pos, @NotNull CollisionContext context) {
 		return SHAPE;
 	}
 
 	@Override
-	public VoxelShape getCollisionShape(BlockState state, BlockGetter getter, BlockPos pos, CollisionContext context) {
+	public @NotNull VoxelShape getCollisionShape(@NotNull BlockState state, @NotNull BlockGetter getter, @NotNull BlockPos pos, @NotNull CollisionContext context) {
 		return SHAPE;
 	}
 
 	@Override
-	public boolean isPossibleToRespawnInThis(BlockState p_279289_) {
+	public boolean isPossibleToRespawnInThis(@NotNull BlockState p_279289_) {
 		return true;
 	}
 
 	@Override
-	public void fallOn(Level level, BlockState state, BlockPos pos, Entity entity, float p_180658_4_) {
+	public void fallOn(@NotNull Level level, @NotNull BlockState state, @NotNull BlockPos pos, @NotNull Entity entity, float p_180658_4_) {
 		//entity.causeFallDamage(p_180658_4_, 0.0F, level.damageSources().fall());
-		return;
 	}
 
 	@Override
-	public void stepOn(Level level, BlockPos pos, BlockState state, Entity entity) {
+	public void stepOn(@NotNull Level level, @NotNull BlockPos pos, @NotNull BlockState state, @NotNull Entity entity) {
 		super.stepOn(level, pos, state, entity);
 	}
 
 	@Override
-	public void entityInside(BlockState state, Level world, BlockPos pos, Entity entity) {
+	public void entityInside(@NotNull BlockState state, @NotNull Level world, @NotNull BlockPos pos, @NotNull Entity entity) {
 		if (collisionEffect == null) {
 			return;
 		}
