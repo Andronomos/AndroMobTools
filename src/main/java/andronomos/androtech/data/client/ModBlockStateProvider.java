6e0b2afa7b60dfb1d03da1216d78a98c1143a50b
Andronomos <andronomos@proton.me>
2024-01-21 03:00:47 -0800

    Removed unused import

--- src/main/java/andronomos/androtech/data/client/ModBlockStateProvider.java
+++ src/main/java/andronomos/androtech/data/client/ModBlockStateProvider.java
@@ -14,7 +14,6 @@ import net.minecraftforge.client.model.generators.ConfiguredModel;
 import net.minecraftforge.client.model.generators.ModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 
 public class ModBlockStateProvider extends BlockStateProvider {
 	public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
