3080cdd03f267947128b8500e7d7930fb170eff5
Andronomos <andronomos@proton.me>
2024-01-24 01:50:34 -0800

    Fixed item incinerator inventory handler not visually deleting items.

--- src/main/java/andronomos/androtech/block/itemincinerator/ItemIncineratorBlockEntity.java
+++ src/main/java/andronomos/androtech/block/itemincinerator/ItemIncineratorBlockEntity.java
@@ -32,6 +32,16 @@ public class ItemIncineratorBlockEntity extends BaseBlockEntity implements MenuP
             }
 
             @Override
+            public @NotNull ItemStack extractItem(int slot, int amount, boolean simulate) {
+                return ItemStack.EMPTY;
+            }
+
+            @Override
+            public @NotNull ItemStack getStackInSlot(int slot) {
+                return ItemStack.EMPTY;
+            }
+
+            @Override
             public int getSlotLimit(int slot) {
                 return 64;
             }
