f0e0fd41a05761e84162e5de8cdb66f20fbce9c2
Andronomos <andronomos@proton.me>
2024-01-27 19:11:52 -0800

    Added applyDamage method

--- src/main/java/andronomos/androtech/util/ItemStackHelper.java
+++ src/main/java/andronomos/androtech/util/ItemStackHelper.java
@@ -2,11 +2,25 @@ package andronomos.androtech.util;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 
 public class ItemStackHelper {
+	public static void applyDamage(LivingEntity player, ItemStack stack, int amount, boolean preventBreaking) {
+		if(preventBreaking) {
+			if(stack.getDamageValue() == stack.getMaxDamage()) {
+				return;
+			}
+			stack.hurt(amount, player.getRandom(), player instanceof ServerPlayer ? (ServerPlayer)player : null);
+		} else {
+			stack.hurtAndBreak(amount, player, (p) -> p.broadcastBreakEvent(InteractionHand.MAIN_HAND));
+		}
+	}
+
 	public static BlockPos getBlockPos(ItemStack stack) {
 		if(stack.isEmpty()) return null;
 		CompoundTag tag = stack.getOrCreateTag();
