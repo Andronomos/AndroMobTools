856833a26c2e7ab351fc601deced08cf4b79ed88
Andronomos <andronomos@proton.me>
2024-02-02 18:19:02 -0800

    Added method for debugging purposes

--- src/main/java/andronomos/androtech/item/base/AbstractDeviceItem.java
+++ src/main/java/andronomos/androtech/item/base/AbstractDeviceItem.java
@@ -1,11 +1,19 @@
 package andronomos.androtech.item.base;
 
+import andronomos.androtech.AndroTech;
 import andronomos.androtech.config.AndroTechConfig;
+import andronomos.androtech.util.ChatHelper;
 import andronomos.androtech.util.ItemStackHelper;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
 
 public abstract class AbstractDeviceItem extends Item {
 	public static int DURABILITY = AndroTechConfig.DEVICE_DURABILITY.get();
@@ -18,6 +26,20 @@ public abstract class AbstractDeviceItem extends Item {
 	public AbstractDeviceItem(Properties properties, boolean hasDurability) {
 		super(properties);
 		this.hasDurability = hasDurability;
+	}
+
+	/**
+	 * For debugging purposes
+	 */
+	@Override
+	public @NotNull InteractionResultHolder<ItemStack> use(Level level, @NotNull Player player, @NotNull InteractionHand hand) {
+		if(!level.isClientSide) {
+			ItemStack stack = player.getItemInHand(hand);
+			CompoundTag tag = stack.getTag();
+			AndroTech.LOGGER.info(String.format("MultiStateItem#use | stack nbt: %s", tag != null ? tag.toString() : "null"));
+			ChatHelper.sendStatusMessage(player, Component.literal(tag != null ? tag.toString() : "null"));
+		}
+		return super.use(level, player, hand);
 	}
 
 	@Override
