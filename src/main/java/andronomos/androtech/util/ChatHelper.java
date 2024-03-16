622aae1739845734f941baaa1c9a82d9c82aef64
Andronomos <andronomos@proton.me>
2024-03-15 19:07:57 -0700

    Removed function from ChatHelper

--- src/main/java/andronomos/androtech/util/ChatHelper.java
+++ src/main/java/andronomos/androtech/util/ChatHelper.java
@@ -1,16 +1,11 @@
 package andronomos.androtech.util;
 
-import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
 
 public class ChatHelper {
-	public static String blockPosToString(BlockPos pos) {
-		return pos.getX() + ", " + pos.getY() + ", " + pos.getZ();
-	}
-
 	public static void sendStatusMessage(Player player, String message) {
-		player.displayClientMessage(Component.translatable(message), true);
+		sendStatusMessage(player, Component.translatable(message));
 	}
 
 	public static void sendStatusMessage(Player player, Component nameTextComponent) {
