c144ddd0b059a717b201c932cc12e056a21f7d99
Andronomos <andronomos@proton.me>
2024-03-22 21:47:17 -0700

    Updated teleport inhibitor model

--- src/main/java/andronomos/androtech/block/TeleportInhibitor.java
+++ src/main/java/andronomos/androtech/block/TeleportInhibitor.java
@@ -9,14 +9,14 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 
 public class TeleportInhibitor extends MachineBlock {
-	private static final VoxelShape SHAPE = Block.box(2, 0, 2, 14, 7, 14);
+	//private static final VoxelShape SHAPE = Block.box(2, 0, 2, 14, 7, 14);
 
 	public TeleportInhibitor(Properties properties) {
 		super(properties);
 	}
 
-	@Override
-	public @NotNull VoxelShape getShape(@NotNull BlockState state, @NotNull BlockGetter getter, @NotNull BlockPos pos, @NotNull CollisionContext context) {
-		return SHAPE;
-	}
+	//@Override
+	//public @NotNull VoxelShape getShape(@NotNull BlockState state, @NotNull BlockGetter getter, @NotNull BlockPos pos, @NotNull CollisionContext context) {
+	//	return SHAPE;
+	//}
 }
