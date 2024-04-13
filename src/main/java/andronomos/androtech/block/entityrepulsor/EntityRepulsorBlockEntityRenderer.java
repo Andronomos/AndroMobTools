b539a2bd7750eac3fc5fd29d1db8eb2f0d4d8eef
Andronomos <andronomos@proton.me>
2024-04-13 13:49:29 -0700

    Created block entity renderer for the Entity Repulsor Block

---
+++ src/main/java/andronomos/androtech/block/entityrepulsor/EntityRepulsorBlockEntityRenderer.java
@@ -0,0 +1,33 @@
+package andronomos.androtech.block.entityrepulsor;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
+import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import org.jetbrains.annotations.NotNull;
+
+public class EntityRepulsorBlockEntityRenderer implements BlockEntityRenderer<EntityRepulsorBlockEntity> {
+	public EntityRepulsorBlockEntityRenderer(BlockEntityRendererProvider.Context context) {
+	}
+
+	@Override
+	public void render(@NotNull EntityRepulsorBlockEntity entity, float partialTicks, @NotNull PoseStack poseStack, @NotNull MultiBufferSource bufferSource, int combinedLight, int combinedOverlay) {
+		if (entity == null || !entity.hasLevel()) {
+			return;
+		}
+
+		if (!entity.showRenderBox) {
+			return;
+		}
+
+		VertexConsumer ivertexbuilder = bufferSource.getBuffer(RenderType.lines());
+		poseStack.pushPose();
+		poseStack.translate(-0.0005D, -0.0005D, -0.0005D);
+		poseStack.scale(0.999F, 0.999F, 0.999F);
+		LevelRenderer.renderLineBox(poseStack, ivertexbuilder, entity.getAABBForRender(), 0F, 0F, 1F, 1F);
+		poseStack.popPose();
+	}
+}
