0f45cdeaa424653b1ce407cc593873f32c098e37
Andronomos <andronomos@proton.me>
2024-04-13 13:47:02 -0700

    Created a new sidebutton for controlling the machine overlay

---
+++ src/main/java/andronomos/androtech/inventory/client/RenderOutlineButton.java
@@ -0,0 +1,23 @@
+package andronomos.androtech.inventory.client;
+
+import net.minecraft.client.gui.components.Tooltip;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.level.block.entity.BlockEntity;
+
+public class RenderOutlineButton extends SideButton {
+	public static final String TOOLTIP_ON = "gui.androtech.overlay_on";
+	public static final String TOOLTIP_OFF = "gui.androtech.overlay_off";
+	private static final TextureEnum TEXTURE_OFF = TextureEnum.OVERLAY_OFF;
+	private static final TextureEnum TEXTURE_ON = TextureEnum.OVERLAY_ON;
+
+	public RenderOutlineButton(OnPress onPress) {
+		super(TEXTURE_OFF, onPress);
+	}
+
+	public void update(boolean showRenderBox) {
+		boolean isOn = showRenderBox;
+		setCurrentTexture(isOn ? TEXTURE_ON : TEXTURE_OFF);
+		Tooltip t = Tooltip.create(Component.translatable(isOn ? TOOLTIP_ON : TOOLTIP_OFF));
+		setTooltip(t);
+	}
+}
