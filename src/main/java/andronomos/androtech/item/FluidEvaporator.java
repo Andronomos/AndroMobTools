59eb5315674e522ae40a959d1e47e0dc70dcb8a0
Andronomos <andronomos@proton.me>
2024-04-19 04:18:59 -0700

    Added tooltip constants for the modes. Removed unnecessary else statement. Removed unused function and imports.

--- src/main/java/andronomos/androtech/item/FluidEvaporator.java
+++ src/main/java/andronomos/androtech/item/FluidEvaporator.java
@@ -22,13 +22,14 @@ import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.material.Fluids;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
 public class FluidEvaporator extends AbstractDeviceItem {
-	public static final String TOOLTIP_FLUID_EVAPORATOR = "tooltip.androtech.fluid_evaporator";
-	public static final String TOOLTIP_FLUID_EVAPORATOR_MODE = "tooltip.androtech.fluid_evaporator.mode";
+	public static final String TOOLTIP = "item.androtech.fluid_evaporator.tooltip";
+	public static final String TOOLTIP_MODE = "item.androtech.fluid_evaporator.tooltip.mode";
+	public static final String TOOLTIP_MODE_WATER = "item.androtech.fluid_evaporator.tooltip.mode.water";
+	public static final String TOOLTIP_MODE_LAVA = "item.androtech.fluid_evaporator.tooltip.mode.lava";
 	private static final int RANGE = AndroTechConfig.FLUID_EVAPORATOR_RANGE.get();
 	private static final String NBT_MODE = "mode";
 	public static final int COOLDOWN = 15;
@@ -80,8 +81,6 @@ public class FluidEvaporator extends AbstractDeviceItem {
 		for (BlockPos posTarget : nearbyFluid) {
 			if (removeFlowingLiquid(level, posTarget)) {
 				countSuccess++;
-			} else {
-				String boop = "";
 			}
 		}
 		if (countSuccess > 0) {
@@ -91,62 +90,6 @@ public class FluidEvaporator extends AbstractDeviceItem {
 				SoundHelper.playSound(level, pos, SoundEvents.FIRE_EXTINGUISH);
 			}
 		}
-	}
-
-	//@Override
-	//public @NotNull InteractionResult useOn(UseOnContext context) {
-	//	BlockPos pos = context.getClickedPos();
-	//	Level world = context.getLevel();
-	//	Direction face = context.getClickedFace();
-	//	ItemStack itemstack = context.getItemInHand();
-	//	EvaporateMode fluidMode = EvaporateMode.values()[itemstack.getOrCreateTag().getInt(NBT_MODE)];
-	//	List<BlockPos> area = cubeSquareBase(pos.relative(face), SIZE, 1);
-	//	int countSuccess = 0;
-	//	boolean tryHere;
-	//	for (BlockPos posTarget : area) {
-	//		BlockState blockHere = world.getBlockState(posTarget);
-	//		FluidState fluidHere = blockHere.getFluidState();
-	//		if (fluidHere == null) {
-	//			continue;
-	//		}
-	//		tryHere = false;
-	//		if (fluidMode == EvaporateMode.WATER && fluidHere.is(FluidTags.WATER)) {
-	//			tryHere = true;
-	//		}
-	//		else if (fluidMode == EvaporateMode.LAVA && fluidHere.is(FluidTags.LAVA)) {
-	//			tryHere = true;
-	//		}
-	//		if (tryHere && removeFlowingLiquid(world, posTarget, true)) {
-	//			countSuccess++;
-	//		}
-	//	}
-	//	if (countSuccess > 0) {
-	//		Player player = context.getPlayer();
-	//		player.swing(context.getHand());
-	//		ItemStackHelper.applyDamage(player, itemstack, 1);
-	//		if (world.isClientSide) {
-	//			SoundHelper.playSound(world, pos, SoundEvents.FIRE_EXTINGUISH);
-	//		}
-	//	}
-	//	return InteractionResult.SUCCESS;
-	//}
-
-	private static List<BlockPos> cubeSquareBase(final BlockPos pos, int radius, int height) {
-		List<BlockPos> shape = new ArrayList<>();
-		// search in a cube
-		int xMin = pos.getX() - radius;
-		int xMax = pos.getX() + radius;
-		int zMin = pos.getZ() - radius;
-		int zMax = pos.getZ() + radius;
-		for (int x = xMin; x <= xMax; x++) {
-			for (int z = zMin; z <= zMax; z++) {
-				for (int y = pos.getY(); y <= pos.getY() + height; y++) {
-					//now go max height on each pillar for sort order
-					shape.add(new BlockPos(x, y, z));
-				}
-			}
-		}
-		return shape;
 	}
 
 	private static boolean removeFlowingLiquid(Level world, BlockPos pos) {
@@ -164,9 +107,7 @@ public class FluidEvaporator extends AbstractDeviceItem {
 
 	private static MutableComponent getModeTooltip(ItemStack stack) {
 		EvaporateMode mode = EvaporateMode.values()[stack.getOrCreateTag().getInt(NBT_MODE)];
-		return Component.translatable(TOOLTIP_FLUID_EVAPORATOR_MODE,
-				Component.translatable(String.format("tooltip.androtech.fluid_evaporator.mode.%s",
-						mode.getSerializedName())));
+		return Component.translatable(TOOLTIP_MODE, Component.translatable(mode.getSerializedName() == "water" ? TOOLTIP_MODE_WATER : TOOLTIP_MODE_LAVA));
 	}
 
 	public static void toggleMode(Player player, ItemStack stack) {
