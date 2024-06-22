80b3789a343dfb51c4c2ada10f59d721247025f7
Andronomos <andronomos@proton.me>
2024-06-22 04:56:20 -0700

    Renamed liquid xp registry object

--- src/main/java/andronomos/androtech/registry/FluidRegistry.java
+++ src/main/java/andronomos/androtech/registry/FluidRegistry.java
@@ -11,14 +11,14 @@ import net.minecraftforge.registries.RegistryObject;
 public class FluidRegistry {
 	public static DeferredRegister<Fluid> FLUIDS = DeferredRegister.create(ForgeRegistries.FLUIDS, AndroTech.MODID);
 
-	public static final RegistryObject<FlowingFluid> LIQUID_XP_SOURCE = FLUIDS.register("liquid_xp_fluid",
+	public static final RegistryObject<FlowingFluid> LIQUID_XP = FLUIDS.register("liquid_xp_fluid",
 			() -> new ForgeFlowingFluid.Source(FluidRegistry.LIQUID_XP_FLUID_PROPERTIES));
 
 	public static final RegistryObject<FlowingFluid> LIQUID_XP_FLOWING = FLUIDS.register("flowing_liquid_xp",
 			() -> new ForgeFlowingFluid.Flowing(FluidRegistry.LIQUID_XP_FLUID_PROPERTIES));
 
 	public static final ForgeFlowingFluid.Properties LIQUID_XP_FLUID_PROPERTIES = new ForgeFlowingFluid.Properties(
-			FluidTypeRegistry.LIQUID_XP_FLUID_TYPE, LIQUID_XP_SOURCE, LIQUID_XP_FLOWING)
+			FluidTypeRegistry.LIQUID_XP_FLUID_TYPE, LIQUID_XP, LIQUID_XP_FLOWING)
 			.slopeFindDistance(2).levelDecreasePerBlock(2).block(BlockRegistry.LIQUID_XP_BLOCK)
 			.bucket(ItemRegistry.LIQUID_XP_BUCKET);
 }
