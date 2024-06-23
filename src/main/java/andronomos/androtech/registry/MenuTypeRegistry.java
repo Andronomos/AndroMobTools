b4f0507f8df202d96d621f56491fee1d206e4595
Andronomos <andronomos@proton.me>
2024-06-23 12:00:20 -0700

    Created experience attractor menu

--- src/main/java/andronomos/androtech/registry/MenuTypeRegistry.java
+++ src/main/java/andronomos/androtech/registry/MenuTypeRegistry.java
@@ -1,6 +1,8 @@
 package andronomos.androtech.registry;
 
 import andronomos.androtech.AndroTech;
+import andronomos.androtech.block.ExperienceAttractor.ExperienceAttractorBlock;
+import andronomos.androtech.block.ExperienceAttractor.ExperienceAttractorMenu;
 import andronomos.androtech.block.mobrepulsor.MobRepulsorMenu;
 import andronomos.androtech.block.itemattractor.ItemAttractorMenu;
 import andronomos.androtech.block.mobkiller.MobKillerMenu;
@@ -20,6 +22,7 @@ public class MenuTypeRegistry {
 	public static final RegistryObject<MenuType<MobKillerMenu>> MOB_KILLER_MENU = register(MobKillerMenu::new, "mob_killer_menu");
 	public static final RegistryObject<MenuType<ItemAttractorMenu>> ITEM_ATTRACTOR_MENU = register(ItemAttractorMenu::new, "item_attractor_menu");
 	public static final RegistryObject<MenuType<ItemIncineratorMenu>> ITEM_INCINERATOR_MENU = register(ItemIncineratorMenu::new, "item_incinerator_menu");
+	public static final RegistryObject<MenuType<ExperienceAttractorMenu>> EXPERIENCE_ATTRACTOR_MENU = register(ExperienceAttractorMenu::new, "experience_attractor_menu");
 	public static final RegistryObject<MenuType<RedstoneSignalTransmitterMenu>> REDSTONE_SIGNAL_TRANSMITTER_MENU = register(RedstoneSignalTransmitterMenu::new, "redstone_signal_transmitter_menu");
 	public static final RegistryObject<MenuType<MobRepulsorMenu>> MOB_REPULSOR_MENU = register(MobRepulsorMenu::new, "mob_repulsor_menu");
 
