88179cec99eae3730c191a6a8654c0c4bb96c40a
Andronomos <andronomos@proton.me>
2024-06-24 00:33:49 -0700

    Fixed fluid tank renderer size
    Moved fluid tank renderer position into constants

--- src/main/java/andronomos/androtech/block/itemattractor/ItemAttractorScreen.java
+++ src/main/java/andronomos/androtech/block/itemattractor/ItemAttractorScreen.java
@@ -2,6 +2,7 @@ package andronomos.androtech.block.itemattractor;
 
 import andronomos.androtech.AndroTech;
 import andronomos.androtech.block.base.BaseScreen;
+import andronomos.androtech.fluid.FluidTankRenderer;
 import andronomos.androtech.inventory.client.PowerButton;
 import andronomos.androtech.network.AndroTechPacketHandler;
 import andronomos.androtech.network.packet.PoweredStateSyncPacket;
@@ -9,14 +10,18 @@ import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
-import net.minecraftforge.fluids.FluidStack;
 import org.jetbrains.annotations.NotNull;
 
 public class ItemAttractorScreen extends BaseScreen<ItemAttractorMenu> {
+	private final int X_OFFSET = 152;
+	private final int Y_OFFSET = 18;
+	private final ItemAttractorBlockEntity entity;
 	private PowerButton powerButton;
+
 
 	public ItemAttractorScreen(ItemAttractorMenu menu, Inventory inventory, Component component) {
 		super(menu, inventory, component);
+		entity = (ItemAttractorBlockEntity)menu.blockEntity;
 	}
 
 	@Override
@@ -24,19 +29,19 @@ public class ItemAttractorScreen extends BaseScreen<ItemAttractorMenu> {
 		super.init();
 		powerButton = (PowerButton)this.addButton(new PowerButton((button) ->
 				AndroTechPacketHandler.sendToServer(new PoweredStateSyncPacket(menu.blockEntity.getBlockPos())), menu.blockEntity));
-		setFluidRenderer(new FluidTankRenderer(64000, 124, 51));
+		setFluidRenderer(new FluidTankRenderer(64000, 16, 52));
 	}
 
 	@Override
 	protected void renderLabels(@NotNull GuiGraphics graphics, int mouseX, int mouseY) {
 		super.renderLabels(graphics, mouseX, mouseY);
 		powerButton.update();
-		renderFluidTooltips(graphics, mouseX, mouseY, 8, 19, entity.getFluidStack());
+		renderFluidTooltips(graphics, mouseX, mouseY, X_OFFSET, Y_OFFSET, entity.getFluidStack());
 	}
 
 	@Override
 	protected void renderBg(@NotNull GuiGraphics guiGraphics, float partialTick, int mouseX, int mouseY) {
 		drawBackground(guiGraphics, new ResourceLocation(AndroTech.MODID, "textures/gui/item_attractor.png"));
-		renderFluid(guiGraphics, 8, 19, entity.getFluidStack());
+		renderFluid(guiGraphics, X_OFFSET, Y_OFFSET, entity.getFluidStack());
 	}
 }
